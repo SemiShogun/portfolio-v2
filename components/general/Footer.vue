afdec94e40d798a7e987aae6b0bb7c4eac747f57
SemiShogun <nectoj@gmail.com>
2020-12-23 20:12:06 +0100

    Removed Description

--- components/general/Footer.vue
+++ components/general/Footer.vue
@@ -3,7 +3,7 @@
     <!-- <ul class="flex flex-row w-full justify-between text-white"> -->
     <ul class="flex flex-row justify-between text-white w-full text-center p-4 pin-b">
       <li class="flex p-1 font-light">
-        <nuxt-link to="mailto:nectoj@gmail.com">Send me an E-Mail!</nuxt-link>
+        <a href="mailto: nectoj@gmail.com">Send me an E-Mail!</a>
       </li>
       <li class="flex p-1 font-light">
         <p>2020 Nectoj.ninja</p>
