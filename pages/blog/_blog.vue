0f021c62549348d75e3ee56070581bb37d0aa0ef
SemiShogun <nectoj@gmail.com>
2021-05-12 18:08:59 +0200

    Add remark config

--- pages/blog/_blog.vue
+++ pages/blog/_blog.vue
@@ -38,15 +38,16 @@ export default {
   },
   mounted() {
     var remark_config = {
-      host: 'https://remark42.nectoj.ninja/', // hostname of remark server, same as REMARK_URL in backend config, e.g. "https://demo.remark42.com"
-      site_id: 'JamieLam1234',
-      components: ['embed'], // optional param; which components to load. default to ["embed"]
+      host: 'https://remark42.nectoj.ninja',
+      // hostname of remark server, same as REMARK_URL in backend config,
+      // e.g. "https://demo.remark42.com" site_id: 'remark42.vidal-rosset.net',components: ['embed', 'last-comments', 'counter'],
+      // optional param; which components to load. default to ["embed"]
       // to load all components define components as ['embed', 'last-comments', 'counter']
       // available component are:
       //     - 'embed': basic comments widget
       //     - 'last-comments': last comments widget, see `Last Comments` section below
       //     - 'counter': counter widget, see `Counter` section below
-      //   url: 'PAGE_URL', // optional param; if it isn't defined
+      // url: '', // optional param; if it isn't defined
       // `window.location.origin + window.location.pathname` will be used,
       //
       // Note that if you use query parameters as significant part of url
@@ -66,29 +67,19 @@ export default {
       // If you deal with query parameters make sure you pass only significant part of it
       // in well defined order
       max_shown_comments: 10, // optional param; if it isn't defined default value (15) will be used
-      theme: 'dark', // optional param; if it isn't defined default value ('light') will be used
-      page_title: 'Moving to Remark42', // optional param; if it isn't defined `document.title` will be used
-      locale: 'en', // set up locale and language, if it isn't defined default value ('en') will be used
-      show_email_subscription: false, // optional param; by default it is `true` and you can see email subscription feature
-      // in interface when enable it from backend side
-      // if you set this param in `false` you will get notifications email notifications as admin
-      // but your users won't have interface for subscription
+      theme: 'light', // optional param; if it isn't defined default value ('light') will be used
+      // page_title: '' // optional param; if it isn't defined `document.title` will be used
     }
 
-    ;(function () {
-      var host = 'https://remark42.nectoj.ninja/' // Your remark42 host
-      var components = ['embed'] // Your choice of remark42 components
-
-      ;(function (c) {
-        for (let i = 0; i < c.length; i++) {
-          const d = document
-          const s = d.createElement('script')
-          s.src = host + '/web/' + c[i] + '.js'
-          s.defer = true
-          ;(d.head || d.body).appendChild(s)
-        }
-      })(components)
-    })
+    ;(function (c) {
+      for (var i = 0; i < c.length; i++) {
+        var d = document,
+          s = d.createElement('script')
+        s.src = remark_config.host + '/web/' + c[i] + '.js'
+        s.defer = true
+        ;(d.head || d.body).appendChild(s)
+      }
+    })(remark_config.components || ['embed', 'last-comments', 'counter'])
   },
 }
 </script>
